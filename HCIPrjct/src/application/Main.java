76ad4b9916d834377c46e424138b89db8d21c50d
Maxence Debert <Maxence Debert@10.205.94.97>
2017-04-04 19:25:22 -0500

    Test commit Max

--- HCIPrjct/src/application/Main.java
+++ HCIPrjct/src/application/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
+			System.out.println("hello");
 			e.printStackTrace();
 		}
 	}
